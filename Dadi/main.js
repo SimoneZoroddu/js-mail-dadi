a6d51a4bf654884fc9f9e00b62211b5639504649
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 17:30:24 +0100

    add comment and some info that want the exercise

--- Dadi/main.js
+++ Dadi/main.js
@@ -1,10 +1,13 @@
+//CREO 2 NUMERI RANDOM 1 PER LUI E 1 PER ME
+
 let your_number = Math.floor((Math.random() * 6) + 1);
 let my_number = Math.floor((Math.random() * 6) + 1);
 
-
+//GLI COMUNICO IL MIO E IL SUO NUMERO
 alert(`Your Number ${your_number}`);
 alert(`My Number ${my_number}`);
 
+// CONTROLLO QUALE IL MAGGIORE E NEL CASO COMUNICO CHE HA PERSO VINTO O PATTA
 if (your_number > my_number) {
     alert('You win')
 } else if (your_number < my_number) {
--- Email/main.js
+++ Email/main.js
@@ -1,17 +1,30 @@
+// CREO ARRAY CON VARIE EMAIL DI INVITATI
+
 const email = ['francesco@gmail.com', 'giacomo@gmail.com', 'marco@gmail.com', 'pietro@gmail.com', 'mario@gmail.com']
 
+// VERIFICO CHE FUNZIONA
 // console.log(email);
 
+// CHIEDO EMAIL ALLA PERSONA
 const your_email = prompt('Verifica tramite email se sei invitato, inserisci la tua email');
 
+// VERIFICO CHE FUNZIONA
 // console.log(your_email);
 
+//CONTROLLO SE L EMAIL DELLA PERSONA FA PARTE DELLA LISTA DI EMAIL NELL ARRAY -1 (FALSO, NON TROVATO) 1 (VERO, TROVATO)
+// let verification = email.indexOf(your_email)
+
+// VERIFICO CHE FUNZIONA
+// console.log(verification);
+
+// PASSAGGIO TRAMITE IF ELSE SENZA FOR QUINDI NON CONTROLLANDO 1 A 1 GLI ELEMENTI MA SOLO VERIFICANDO TRAMITE INDEX SE PRESENTE
 // if (email.indexOf(your_email)) {
 //     alert('Non sei invitato alla mia festa');    
 // } else {
 //     alert('Sei invitato alla mia festa');
 // }
 
+//TRAMITE FOR CONTROLLO OGNI EMAIL E OGNI VOLTA CONTROLLA SE PRESENTE L EMAIL INSERITA DALLA PERSONA, NEL CASO LA TROVA FA DIVENTARE LA VARIABILE GLOBALE CREATA ALL ESTERNO POSITICAMENTE
 let accept = 'Non sei invitato alla mia festa'
 
 for (let i = 0; i < email.length; i++) {
@@ -22,6 +35,6 @@ for (let i = 0; i < email.length; i++) {
     }
 }
 
-
+// GLI DO L ESITO ALLA PERSONA
 alert(accept);
 
