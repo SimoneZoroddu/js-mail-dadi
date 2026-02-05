2075e5fd63d78f9342067b036b6a9bd042a0dd1f
SimoneZoroddu <lenviur@gmail.com>
2026-02-05 16:31:51 +0100

    adding email and prompt with your_email

---
+++ main.js
@@ -0,0 +1,8 @@
+const email = ['francesco@gmail.com', 'giacomo@gmail.com', 'marco@gmail.com', 'pietro@gmail.com', 'mario@gmail.com']
+
+// console.log(email);
+
+const your_email = prompt('Verifica tramite email se sei invitato, inserisci la tua email');
+
+
+//console.log(your_email);
